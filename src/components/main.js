18482e7740b301ba6d421552d47acc21db440d17
Marchi <marchigomes@gmail.com>
2025-02-27 16:47:27 -0300

    fc

---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+
+function Mainconteiner (){
+    return(
+
+        <div className="conteiner">
+                <img src="https://www.worldometers.info/img/flags/br-flag.gif" className="flg_img"></img>
+            <div className="op_conteiner">
+                <div className="options">Albaia</div>
+                <div className="options">brasil</div>
+                <div className="options">afghanistan</div>
+                <div className="options">swana</div>
+            </div>
+        </div>
+
+    );
+}
+
+
+
+export default Mainconteiner;
